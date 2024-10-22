c5e428964e7f9892f899bde4ad2c7d46af771423
Alex Larin <alex.larin@inbox.ru>
2024-10-22 16:23:54 +0300

    some edits

--- hw2/src/test/java/ru/axe/hw2/ProgramTests.java
+++ hw2/src/test/java/ru/axe/hw2/ProgramTests.java
@@ -3,7 +3,7 @@ package ru.axe.hw2;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Test {
+class ProgramTests {
 
 	@org.junit.jupiter.api.Test
 	void contextLoads() {
